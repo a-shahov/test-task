5a6ec24c3518531b733a8253f3aa4f6a2e0339a4
andrey <a.shahov.garlic@yandex.ru>
2024-05-28 15:55:23 +0500

    feat: cli added

--- src/alt_binary_packages/__main__.py
+++ src/alt_binary_packages/__main__.py
@@ -1,6 +1,20 @@
+import asyncio
+import argparse
+
+import aiohttp
+
+from .const import ARCHITECTURES
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "arch",
+        choices=ARCHITECTURES,
+        help="arch is desired architecture.",
+    )
+    args = parser.parse_args()
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
---
+++ src/alt_binary_packages/const.py
@@ -0,0 +1,3 @@
+ARCHITECTURES = ["x86_64", "i586", "noarch", "aarch64", "ppc64le", "srpm"]
+URL = "https://rdb.altlinux.org/api/export/branch_binary_packages/"
+
