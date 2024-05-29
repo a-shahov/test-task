aeea51219721a51c52d4e5847425b80764495988
andrey <a.shahov.garlic@yandex.ru>
2024-05-29 15:56:56 +0500

    fix: remove srpm from available arch

--- src/alt_binary_packages/const.py
+++ src/alt_binary_packages/const.py
@@ -1,4 +1,4 @@
-ARCHITECTURES = ['x86_64', 'i586', 'noarch', 'aarch64', 'ppc64le', 'srpm']
+ARCHITECTURES = ['x86_64', 'i586', 'noarch', 'aarch64', 'ppc64le']
 URL = 'https://rdb.altlinux.org/api/export/branch_binary_packages/{}'
 TIMEOUT = 8  # timeout for making a request in seconds
 
